51d6a127bee7bd682fb83763e6363588bbbdf341
mo8047 <itshassan45@gmail.com>
2026-04-05 23:26:44 -0500

    assignment 4 expense tracker

---
+++ docs/server/angular-app-engine-manifest.mjs
@@ -0,0 +1,11 @@
+
+export default {
+  basePath: '/assignment-4-expense-tracker',
+  allowedHosts: [],
+  supportedLocales: {
+  "en-US": ""
+},
+  entryPoints: {
+    '': () => import('./main.server.mjs')
+  },
+};
